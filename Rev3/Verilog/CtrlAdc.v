d82bc9a13e71da45b1b385e7a72a1594a08637d6
Zihan Chen <zihan.chen@jhu.edu>
2013-08-13 01:26:22 +0000

    FPGA1394_QLA: created tag for Firmware Rev3
    git-svn-id: https://svn.lcsr.jhu.edu/mechatronics/tags/Firmware/FPGA1394_QLA@145 95aa9265-a27f-408e-9981-efeafd1b3a66

---
+++ Rev3/Verilog/CtrlAdc.v
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ *
+ * Copyright(C) 2011 ERC CISST, Johns Hopkins University.
+ *
+ * This module controls access to the adc modules by selecting the data to
+ * output based on the read address, and by combining the 16-bit values for
+ * analog pot and motor current into a single 32-bit word.
+ *
+ * Revision history
+ *     11/22/11    Paul Thienphrapa    Initial revision
+ */
+
+// device register file offsets from channel base
+`define OFF_ADC_DATA 4'd0          // adc data register offset (pot + cur)
+`define OFF_DAC_CTRL 4'd1          // dac control register offset
+`define OFF_POT_CTRL 4'd2          // pot control register offset
+`define OFF_POT_DATA 4'd3          // pot data register offset
+`define OFF_ENC_LOAD 4'd4          // enc data preload offset
+`define OFF_ENC_DATA 4'd5          // enc quadrature register offset
+`define OFF_PER_DATA 4'd6          // enc period register offset
+`define OFF_FREQ_DATA 4'd7         // enc frequency register offset
+
+
+module CtrlAdc(
+    input  wire clkadc,            // adc clock
+    input  wire reset,             // system reset
+    output wire[1:2] sclk,         // sclk signal to each set of adcs
+    output wire[1:2] conv,         // conv signal to each set of adcs
+    input  wire[1:8] miso,         // data lines from each individual adc
+    input  wire[7:0] reg_addr,     // register file addr from outside world
+    output wire[31:0] reg_rdata,   // outgoing register file data
+    output wire[15:0] cur1,        // current axis 1
+    output wire[15:0] cur2,        // current axis 2
+    output wire[15:0] cur3,        // current axis 3
+    output wire[15:0] cur4         // current axis 4
+);
+
+
+    // local wires
+    wire[1:4] miso_pot;
+    wire[1:4] miso_cur;
+    wire[15:0] potval[0:15];       // 4 channels of analog pot values
+    wire[15:0] curval[0:15];       // 4 channels of current feedback values
+
+    // for array-style access to the adc data
+    wire[31:0] mem_data[0:15][0:15];
+
+//------------------------------------------------------------------------------
+// hardware description
+//
+
+// assign miso to local miso wire
+assign miso_pot = miso[1:4];
+assign miso_cur = miso[5:8];
+
+// output selected read register
+assign reg_rdata = mem_data[reg_addr[7:4]][reg_addr[3:0]];
+
+
+// pot feedback module
+Ltc1864x4 adc_pot(
+    .clk(clkadc),
+    .reset(reset),
+    .Out1(potval[1]),
+    .Out2(potval[2]),
+    .Out3(potval[3]),
+    .Out4(potval[4]),
+    .sclk(sclk[1]),
+    .conv(conv[1]),
+    .miso(miso_pot)
+);
+
+// cur feedback module
+Ltc1864x4 adc_cur(
+    .clk(clkadc),
+    .reset(reset),
+    .Out1(curval[1]),
+    .Out2(curval[2]),
+    .Out3(curval[3]),
+    .Out4(curval[4]),
+    .sclk(sclk[2]),
+    .conv(conv[2]),
+    .miso(miso_cur)
+);
+
+// map the data lines to access them as memory: [channel #][device #]
+// channel 1
+assign mem_data[1][`OFF_ADC_DATA] = { potval[1], curval[1] };
+// channel 2
+assign mem_data[2][`OFF_ADC_DATA] = { potval[2], curval[2] };
+// channel 3
+assign mem_data[3][`OFF_ADC_DATA] = { potval[3], curval[3] };
+// channel 4
+assign mem_data[4][`OFF_ADC_DATA] = { potval[4], curval[4] };
+
+// connect to cur value output
+assign cur1 = curval[1];
+assign cur2 = curval[2];
+assign cur3 = curval[3];
+assign cur4 = curval[4];
+
+endmodule
---
+++ Rev3/Verilog/SafetyCheck.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+
+/*******************************************************************************
+ *
+ * Copyright(C) 2013 ERC CISST, Johns Hopkins University.
+ *
+ * This module controls access to the adc modules by selecting the data to
+ * output based on the read address, and by combining the 16-bit values for
+ * analog pot and motor current into a single 32-bit word.
+ *
+ * Revision history
+ *     04/26/13    Zihan Chen    Initial revision
+ */
+
+
+
+module SafetyCheck(
+    input  wire clk,            // system clock
+    input  wire reset,          // global reset
+    input  wire[15:0] cur_in,   // feedback current
+    input  wire[15:0] dac_in,   // command current
+    input  wire reg_wen, 
+    output reg  amp_disable     // amplifier disable
+    );
+     
+    // local variable
+    reg [23:0] error_counter;  // error counter 
+    reg [15:0] abs_error_cur;
+    wire [15:0] high_limit;
+    wire[15:0] low_limit;
+	 
+    // ---- Code Starts Here -----
+    initial begin
+        amp_disable <= 1'b0;
+        error_counter <= 24'd0;
+    end
+    
+    assign high_limit = ((dac_in[15] == 1'b1) ? dac_in : ~dac_in) + 16'h0900;
+    assign low_limit = ((dac_in[15] == 1'b1) ? ~dac_in : dac_in) - 16'h0900;
+
+    always @ (posedge clk)
+    begin
+        // If measured current is small (150 mA), clear error counter
+        if ((cur_in < 16'h8300) && (cur_in > 16'h7d00)) begin
+           error_counter <= 24'd0;
+        end
+        
+        // else if commanded current is large, 
+        // clear error counter (margin = 0x0900 440 mA)
+        else if ((dac_in <= 16'h0900) || (dac_in >= 16'hf6ff)) begin
+           error_counter <= 24'd0;
+        end 
+          
+        // else perform safety check
+        else begin
+           if ((cur_in < low_limit) || (cur_in > high_limit)) begin
+               error_counter <= error_counter + 1;
+           end
+           else begin
+               error_counter <= 24'd0;
+           end
+        end
+    end
+
+    
+    // amp_disable
+    always @ (posedge(clk) or negedge(reset) or posedge(reg_wen))
+    begin
+        if (reset == 0 || reg_wen) begin
+            amp_disable <= 1'b0;
+        end
+        
+        // 50 mS 
+        else if (error_counter < 24'd2457600) begin
+        // for simulation
+//        else if (error_counter < 24'd5) begin 
+            amp_disable <= amp_disable;
+        end
+        else begin
+            amp_disable <= 1'b1;
+        end
+    end
+    
+endmodule
